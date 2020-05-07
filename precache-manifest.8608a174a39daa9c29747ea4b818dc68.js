5c82ddf88c452efba4ced2acd1e48e965fe6e563
amal-san <amaldemaniax@gmail.com>
2020-05-07 14:33:55 +0530

    Updates

---
+++ precache-manifest.8608a174a39daa9c29747ea4b818dc68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9769c4df9b04691bf70fe5a13c7fcfc7",
+    "url": "/typerex/index.html"
+  },
+  {
+    "revision": "0de522dfc719fcf259a4",
+    "url": "/typerex/static/css/main.60338112.chunk.css"
+  },
+  {
+    "revision": "d81e132b6a7f05031f45",
+    "url": "/typerex/static/js/2.a36a5426.chunk.js"
+  },
+  {
+    "revision": "ec814dca226ec610acb2358fc67425d6",
+    "url": "/typerex/static/js/2.a36a5426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de522dfc719fcf259a4",
+    "url": "/typerex/static/js/main.9fb291de.chunk.js"
+  },
+  {
+    "revision": "14f3d26fe3bcfbd56929",
+    "url": "/typerex/static/js/runtime-main.dbf7c82f.js"
+  }
+]);
