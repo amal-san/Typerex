f2a4d9330a7ef145228120877a8a5e16c630e4e2
amal-san <amaldemaniax@gmail.com>
2020-05-08 08:46:06 +0530

    logout context menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,27 @@
 import React from "react"
 import '../index.css';
+import { Menu, Item, Separator, Submenu, MenuProvider } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
+
+
+const onClick = ({ props }) => {
+
+	localStorage.removeItem('typerex_username')
+	localStorage.removeItem('wpm')
+	localStorage.removeItem('isLoggedin')
+	window.location.href = '/Typerex/';
+};
+
+
+const LogoutMenu = () => (
+    <Menu id='menu_id' >
+       <Item onClick={onClick}>Logout</Item>
+   </Menu>
+);
 
 
 
-
-
-class Header extends React.Component {
-
-	constructor(){
-
-		super()
-
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-
-
-	handleClick() {
-		localStorage.removeItem('typerex_username')
-		localStorage.removeItem('wpm')
-		localStorage.removeItem('isLoggedin')
-		window.location.href = '/Typerex/';
-
-
-	}
-
-
-	render() {
-		return(
+const Header = (props) => (
 		<nav>
 			<div className="nav">
 				<div className='user-item'></div>
@@ -40,15 +34,46 @@ class Header extends React.Component {
 						typerex
 					</a>
 				</div>
-				<div className='user-item-main' onClick={this.handleClick}>
+				<div className='user-item-main'>
 					<div className='user-item-sub'>
+					<MenuProvider id="menu_id">
+						<>
 						<p style={{marginBottom:'0'}}><b id='username'></b></p>
 						<p id='user-wpm' style={{margin:'0'}}></p>
+						<LogoutMenu />
+						</>
+					</MenuProvider>
 					</div>
 				 </div>
 			</div>
-		</nav>)
-	}
-}
+		</nav>
+
+
+
+	)
+
+// class Header extends React.Component {
+
+// 	// constructor(){
+
+// 	// 	super()
+
+// 	// 	this.handleClick = this.handleClick.bind(this);
+// 	// }
+
+
+
+// 	// handleClick() {
+
+
+
+// 	// }
+
+
+// 	render() {
+// 		return(
+// 		)
+// 	}
+// }
 
 export default Header
